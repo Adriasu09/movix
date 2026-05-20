f2d19fb5188f4f14970bc44081d48b575a3b19ed
Adriana Suárez <adsuarez09@gmail.com>
2026-05-20 22:37:24 +0200

    format proyect

--- src/app/providers/QueryProvider.jsx
+++ src/app/providers/QueryProvider.jsx
@@ -12,7 +12,5 @@ const queryClient = new QueryClient({
 });
 
 export const QueryProvider = ({ children }) => {
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
